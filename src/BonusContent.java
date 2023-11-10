29fb7d6dfe8f67be556df07356f0da92b3a62c1f
vassilismylo <t8220103@aueb.gr>
2023-11-10 20:02:41 +0200

    Create BonusContent.java

---
+++ src/BonusContent.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+public class BonusContent {
+       
+	Movie movie = new Movie();//dhmioyrgia antikeimenoy klashs Movie
+
+    
+    String movieName = movie.getMovieId();//Pairnw to movie id apo thn klash Movie
+       
+       //apo edw k katw skip
+
+        public void BonusContent() {
+            try {
+               
+                Document document = Jsoup.connect("https://www.google.com/" + movieName).get();// tha xrhsimopoihsw brave
+
+               
+               
+                Elements funFacts = document.select(".fun-facts");
+
+                if (!funFacts.isEmpty()) {
+                    System.out.println("Βρέθηκαν Fun Facts: " + funFacts.text());
+                   
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
