1f19fa3c67875d1f58da57720909f32a257a056d
NickRagousis <t8210127@aueb.gr>
2023-11-11 16:45:42 +0200

    Movie Class

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 public class Movie {
+    
+    private final String releaseDate;
+    private final String title;
     private final int movieID;
     private final String description;
+    //not sure ArrayLists will be used
     private ArrayList <String> ratings = new ArrayList<String>();
     private ArrayList <String> genres = new ArrayList<String>();
+    private ArrayList <String> reviews = new ArrayList<String>();
     private ArrayList <String> actors = new ArrayList<String>();
-
+    private ArrayList <String> directors = new ArrayList<String>();
+    private ArrayList <String> writers = new ArrayList<String>();
+    private ArrayList <String> producers = new ArrayList<String>();
+   
+   
+    //unfinished
     public Movie(int movieID, String title, String releaseDate, String description) {
         this.movieID = movieID;
         this.title = title;
@@ -21,15 +31,26 @@ public class Movie {
         return title;
     }
 
-    private final String title;
+    
     public String getReleaseDate() {
         return releaseDate;
     }
 
-    private final String releaseDate;
+    
     public String getDescription() {
         return description;
     }
 
+    //public String getDetails() {}
 
+    //public String getRecommendation(){}
+
+    //public double getRating(){}
+
+    //public String getAvailability() {}
+
+    //public ?? getBonusContent() {}
+    
+    //pulic void filterReviewSpoilers
+    
 }
