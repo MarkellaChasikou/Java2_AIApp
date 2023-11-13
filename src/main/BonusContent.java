d3471d4a62d11ceb633980bde82c5ee1626cb8bb
Aglag257 <t8210079@aueb.gr>
2023-11-13 02:04:54 +0200

    putting files in the correct folders

--- src/main/BonusContent.java
+++ src/main/BonusContent.java
@@ -1,4 +1,4 @@
-package test;
+package main;
 import java.io.IOException;
 import java.util.Scanner;
 import org.jsoup.Jsoup;
---
+++ src/main/Review.java
@@ -0,0 +1,74 @@
+package main;
+public class Review {
+    private int reviewID;
+    private int userID;
+    private int movieID;
+    private float rating;
+    private String reviewText;
+    private boolean isSpoiler;
+    
+//Constructor
+    public Review(int reviewID, int userID, int movieID, float rating, String reviewTextext, boolean isSpoiler) {
+        this.reviewID = reviewID;
+        this.userID = userID;
+        this.movieID = movieID;
+        this.rating = rating;
+        this.reviewText = reviewTextext;
+        this.isSpoiler = isSpoiler;
+    }
+
+//Getter methods
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+    public String GetReviewText() {
+        return reviewText;
+    }
+    public boolean GetIsSpoiler() {
+        return isSpoiler;
+    }
+
+//Setter methods
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+    public void SetReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+//Other Methods
+    public void editReview(String reviewText) {
+
+    }
+    public void checkSpoiler(boolean isSpoiler, String reviewText) {
+        if (isSpoiler) {
+
+        } else {
+
+        }
+    }
+    public void deleteReview() {
+
+    }
+}
