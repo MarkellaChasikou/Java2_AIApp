0c76eb2ea6da562698b670134c9a498612068b73
NickRagousis <t8210127@aueb.gr>
2023-11-18 22:30:26 +0200

    App, Cast, Crew, Genre, Contributors, MovieDetails classes added. Movie class updated.

---
+++ src/main/Genre.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Genre {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre [name=" + name + "]";
+    }
+}
