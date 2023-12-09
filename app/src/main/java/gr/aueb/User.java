816af2400af51cfe007d5e9ea4b2fb7d83a2dc82
Aglag257 <t8210079@aueb.gr>
2023-12-09 16:26:16 +0200

    Moved files to correct folders

--- app/src/main/java/gr/aueb/User.java
+++ app/src/main/java/gr/aueb/User.java
@@ -1,12 +1,11 @@
 package gr.aueb;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
+import java.sql.*;
 
 
 public class User {
-
-    private int userID;
     private String username;
     private String password;
     private List<String> followers;
@@ -14,35 +13,26 @@ public class User {
     private List<String> favorites;
     private List<String> watchList;
     private List<String> createdList;
-    private static List<User> allUsers = new ArrayList<>();
-    private static HashMap<String, String> userCredentials = new HashMap<>();
-
     
-    //Constructor
+    //Constructors
+
+    public User(){ }
+
     public User(String username, String password) {
-        boolean check = checkUserExistance(username);
-        if (check == false) {
-            this.userID = allUsers.size() +1 ;
             this.username = username;
             this.password = password;
-            this.followers = new ArrayList<>();
+            /*this.followers = new ArrayList<>();
             this.following = new ArrayList<>();
             this.favorites = new ArrayList<>();
             this.watchList = new ArrayList<>();
             this.createdList = new ArrayList<>();
-            allUsers.add(this);
-            userCredentials.put(username, password);
+            */
         }
-        else {
-            System.out.println("Username " + username + " is not available");
-        }
-    }
 
     //User toString() method
     @Override
     public String toString() {
-        return "\nUserID:" + this.userID + 
-                "\nUsername:" + this.username + 
+        return  "\nUsername:" + this.username + 
                 "\nPassword:" + this.password +
                 "\nFollowers:" + this.followers + "total followers:" + followers.size() +
                 "\nFollowing:" + this.following + "total following:" + following.size() +
@@ -51,90 +41,17 @@ public class User {
                 "\nUser Lists:" + this.createdList;
     }
 
-    //Method returns all Users
-    public static List<User> getAllUsers() {
-        return allUsers;
-    }
-
-    //Method returns all User's Credentials
-    public static HashMap<String, String> getUsersCredentials() {
-        return userCredentials;
-    }
-
-    /*Method getUser checks if specific username exists and return the user
-        if doesn't exist throws exception*/ 
-    public static User getUser(String username) throws UserNotFoundException {
-        for (User user : allUsers) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            } 
-        }
-            throw new UserNotFoundException("There is no User with username:" +username);
-    }
-
-    //Method checks if user exists, if user exists returns true
-    public static boolean checkUserExistance(String username) {
-        boolean check = false; 
-        for (User user: allUsers) {
-            if ( user.getUsername() == username) {
-                check = true;
-            }
-        }
-        return check;
-    }
-
-    //Method to LOG IN
-    public void logIn(String username, String password) {
-        if (userCredentials.containsKey(username)) {
-            String storedPassword = userCredentials.get(username);
-            if (storedPassword.equals(password)) {
-                System.out.println(username +" you are now logged in FILMBRO!!!");
-            }
-            else {
-                System.out.println ("Password does not match.Try again!");
-            }
-        }   else {
-            System.out.println("User: " + username + " does not exist\nPlease,try again!");
-        }
-    }
-    //Method checks if user exists in usercredentials Hashmap
-    public boolean checkLogIn(String username, String password) {
-        if (userCredentials.containsKey(username)) {
-            String storedPassword = userCredentials.get(username);
-            if (storedPassword.equals(password)) 
-                return true;
-            else 
-                return false;
-            
-        } else {
-                return false;
-        }
-    }
-
     
     // Setters
-    public void setUsername(String current_username, String current_password, String new_username) {
-        if (current_username.equals(username) && current_password.equals(password)) {
-        this.username = new_username;
-        } else {
-            System.out.println("Wrong credentials.If you want to change your username "+
-             "you have to give the correct username and password");
-        }
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setPassword(String current_username, String current_password, String new_password) {
-        if (current_username.equals(username) && current_password.equals(password)) {
-        this.password = new_password;
-        } else {
-            System.out.println("Wrong credentials.If you want to change your password "+
-             "you have to give the correct username and password");
-        }
+    public void setPassword( String password) {
+       this.password = password;
     }
 
     //Getters
-    public int getUserID() {
-        return userID;
-    }
 
     public String getUsername() {
         return username;
@@ -190,5 +107,102 @@ public class User {
             System.out.println(username + " you are already follow: " + user.getUsername());
         }
     }
-   
+
+   // Authenticate Method
+    public User authenticate(String username, String password) throws Exception {
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT * FROM users WHERE username=? AND password=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (!rs.next()) {
+                rs.close();
+                stmt.close();
+                db.close();
+                throw new Exception("Wrong username or password");
+            }
+            User user = new User(rs.getString("username"),
+                        rs.getString("password"));
+            rs.close();
+            stmt.close();
+            db.close();
+            return user;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+    
+    //Register Method
+    public void register(User user) throws Exception {	
+		DB db = new DB();
+		Connection con = null;
+		String query = "SELECT * FROM users WHERE username=?";
+		String sql = "INSERT INTO users (username, password) VALUES(?, ?);";
+		try {
+			con = db.getConnection();
+			PreparedStatement stmt1 = con.prepareStatement(query);
+			stmt1.setString(1, user.getUsername());
+			ResultSet rs = stmt1.executeQuery();
+			if (rs.next()) {
+				rs.close();
+				stmt1.close();
+				db.close();
+				throw new Exception("Sorry, username " + user.getUsername() + " is already used.Please choose another one");
+			}
+			PreparedStatement stmt2 = con.prepareStatement(sql);
+			stmt2.setString(1, user.getUsername());
+			stmt2.setString(2, user.getPassword());
+			stmt2.executeUpdate();
+			stmt2.close();
+		} catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+    //Get UserId Method 
+    public int getUserid(String username) throws Exception {
+        DB db = new DB();
+		Connection con = null;
+        String query = "SELECT user_id FROM users WHERE username=?";
+        try {
+			con = db.getConnection();
+			PreparedStatement stmt1 = con.prepareStatement(query);
+			stmt1.setString(1, username);
+			ResultSet rs = stmt1.executeQuery();
+            if (!rs.next()) {
+				rs.close();
+				stmt1.close();
+				db.close();
+				throw new Exception("There is no user " + username);
+			}
+            return rs.getInt("user_id");
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+	}
+    
 }
+
+
+
