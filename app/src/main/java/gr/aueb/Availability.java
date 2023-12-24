1baaa2f768956b1f9cd0113ddce9a635de7ffc0b
NickRagousis <t8210127@aueb.gr>
2023-12-24 14:07:41 +0200

    toString modified, unfinished

--- app/src/main/java/gr/aueb/Availability.java
+++ app/src/main/java/gr/aueb/Availability.java
@@ -1,5 +1,6 @@
 package gr.aueb;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import com.google.gson.annotations.SerializedName;
 
@@ -16,8 +17,70 @@ public class Availability {
 
     }
 
-    @Override
-    public String toString() {
-        return "Availability [results=" + results + "]";
+
+    public String toString(boolean allCountries, boolean myServices, Country c) {
+        String free = null;
+        String ads = null;
+        String buy = null;
+        String flatrate = null;
+        String rent = null;
+
+        if(allCountries == false && myServices == false) {
+            if(!c.getFree().isEmpty()) {
+                free = "Free: \n";
+                for (Prov f : c.getFree()) {
+                    free += f.toString();
+                }
+            }
+            
+            if(!c.getAds().isEmpty()) {
+                ads = "Ads: \n";
+                for (Prov a : c.getAds()) {
+                    ads += a.toString();
+                }
+            }
+            
+            if(!c.getBuy().isEmpty()) {
+                buy = "Buy: \n";
+                for (Prov b : c.getBuy()) {
+                    buy += b.toString();
+                } 
+            }
+
+            if(!c.getFlatrate().isEmpty()) {
+                flatrate = "Stream: ";
+                for (Prov s : c.getFlatrate()) {
+                    flatrate += s.toString();
+                }
+            }
+            
+            
+            if(!c.getAds().isEmpty()) {
+                rent = "Rent: ";
+                for (Prov r : c.getRent()) {
+                    rent += r.toString();
+                }
+            }
+
+            if(free == null && ads == null && buy == null && flatrate == null && rent == null) {
+                return "Not available in " + c;
+            } else return "";
+            
+        
+            //Unfinished
+        
+        } else if(allCountries == true && myServices == false) {
+            for (Country co : results.values()) {
+                for (Prov f : c.getFree()) {
+                    
+                }
+            }
+            
+            return "";
+        } else if(allCountries == false && myServices == false) {
+            return "";
+        } else {
+            return "";
+        }
     }
 }
