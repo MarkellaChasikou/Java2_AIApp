51e4da0506cf0e04910dffd13d4539a0ee0d4ecc
Markella Chasikou <147842164+MarkellaChasikou@users.noreply.github.com>
2024-01-02 22:23:45 +0200

    Add files via upload

--- User.java
+++ User.java
@@ -1,56 +1,45 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
-
 public class User {
+    private int id;
     private String username;
     private String password;
-    private List<String> followers;
-    private List<String> following;
-    private List<String> favorites;
-    private List<String> watchList;
-    private List<String> createdList;
+    private String country;
     
-    //Constructors
-
-    public User(){ }
-
-    public User(String username, String password) {
-            this.username = username;
-            this.password = password;
-            /*this.followers = new ArrayList<>();
-            this.following = new ArrayList<>();
-            this.favorites = new ArrayList<>();
-            this.watchList = new ArrayList<>();
-            this.createdList = new ArrayList<>();
-            */
+//Constructors
+    public User(String username, String password, String country) {
+        this.username = username;
+        this.password = password;
+        this.country = country;
         }
 
-    //User toString() method
-    @Override
-    public String toString() {
-        return  "\nUsername:" + this.username + 
-                "\nPassword:" + this.password +
-                "\nFollowers:" + this.followers + "total followers:" + followers.size() +
-                "\nFollowing:" + this.following + "total following:" + following.size() +
-                "\nFavorites:" + this.favorites + 
-                "\nWatch List:" + this.watchList + 
-                "\nUser Lists:" + this.createdList;
-    }
-
-    
+    public User(int id, String username, String password, String country) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.country = country;
+        }
+ 
     // Setters
+    
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setPassword( String password) {
+    public void setPassword(String password) {
        this.password = password;
     }
 
+    public void setCountry(String country) {
+       this.country = country;
+    }
+
     //Getters
+     public int getId() {
+        return id;
+    }
 
     public String getUsername() {
         return username;
@@ -60,58 +49,15 @@ public class User {
         return password;
     }
 
-    public List<String> getFollowers() {
-        return followers;
+    public String getCountry() {
+        return country;
     }
-
-    public List<String> getFollowing() {
-        return following;
-    }
-
-    public List<String> getFavoriteList() {
-        return favorites;
-    }
-
-    public List<String> getWatchList() {
-        return watchList;
-    }
-
-    public List<String> getCreatedList() {
-        return createdList;
-    }
-
-    //Add to favorites
-    public void addFavorite(String movie) {
-        favorites.add(movie);
-    }
-    //Add to watch list
-    public void addWatchlist(String movie) {
-        watchList.add(movie);
-    }
-    //Create a list
-    public void creatList(String listName) {
-        createdList.add(listName);
-    }
-    //Delete a list
-    public void deleteList(String listName) {
-        createdList.remove(listName);
-    }
-    //Follow user
-    public void followUser(User user) {
-        if (following.contains(user.getUsername()) == false) {
-        following.add(user.getUsername());
-        user.followers.add(this.getUsername());
-        }
-        else {
-            System.out.println(username + " you are already follow: " + user.getUsername());
-        }
-    }
-
-   // Authenticate Method
-    public User authenticate(String username, String password) throws Exception {
+    
+// Login Method
+    public static User login(String username, String password) throws Exception {
         DB db = new DB();
         Connection con = null;
-        String query = "SELECT * FROM users WHERE username=? AND password=?;";
+        String query = "SELECT * FROM AppUser WHERE username=? AND pass_word=?;";
         try {
             con = db.getConnection();
             PreparedStatement stmt = con.prepareStatement(query);
@@ -122,10 +68,12 @@ public class User {
                 rs.close();
                 stmt.close();
                 db.close();
-                throw new Exception("Wrong username or password");
+                return null;   
             }
-            User user = new User(rs.getString("username"),
-                        rs.getString("password"));
+            User user = new User(rs.getInt("userId"),
+                    rs.getString("username"),
+                    rs.getString("pass_word"),
+                    rs.getString("country"));
             rs.close();
             stmt.close();
             db.close();
@@ -136,60 +84,34 @@ public class User {
             try {
                 db.close();
             } catch (Exception e) {
-                
+            
             }
         }
     }
-    
-    //Register Method
-    public void register(User user) throws Exception {	
-		DB db = new DB();
-		Connection con = null;
-		String query = "SELECT * FROM users WHERE username=?";
-		String sql = "INSERT INTO users (username, password) VALUES(?, ?);";
-		try {
-			con = db.getConnection();
-			PreparedStatement stmt1 = con.prepareStatement(query);
-			stmt1.setString(1, user.getUsername());
-			ResultSet rs = stmt1.executeQuery();
-			if (rs.next()) {
-				rs.close();
-				stmt1.close();
-				db.close();
-				throw new Exception("Sorry, username " + user.getUsername() + " is already used.Please choose another one");
-			}
-			PreparedStatement stmt2 = con.prepareStatement(sql);
-			stmt2.setString(1, user.getUsername());
-			stmt2.setString(2, user.getPassword());
-			stmt2.executeUpdate();
-			stmt2.close();
-		} catch (Exception e) {
-            throw new Exception(e.getMessage());
-        } finally {
-            try {
-                db.close();
-            } catch (Exception e) {
-                
-            }
-        }
-    }
-    //Get UserId Method 
-    public int getUserid(String username) throws Exception {
+
+//Register Method
+    public static void register(User user) throws Exception {	
         DB db = new DB();
-		Connection con = null;
-        String query = "SELECT user_id FROM users WHERE username=?";
+        Connection con = null;
+        String query = "SELECT * FROM AppUser WHERE username=?";
+        String sql = "INSERT INTO AppUser (username, pass_word, country) VALUES(?, ?, ?);";
         try {
-			con = db.getConnection();
-			PreparedStatement stmt1 = con.prepareStatement(query);
-			stmt1.setString(1, username);
-			ResultSet rs = stmt1.executeQuery();
-            if (!rs.next()) {
-				rs.close();
-				stmt1.close();
-				db.close();
-				throw new Exception("There is no user " + username);
-			}
-            return rs.getInt("user_id");
+            con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query);
+            stmt1.setString(1, user.getUsername());
+            ResultSet rs = stmt1.executeQuery();
+            if (rs.next()) {
+                rs.close();
+                stmt1.close();
+                db.close();
+                throw new Exception("Sorry, username already registered");
+            }
+            PreparedStatement stmt2 = con.prepareStatement(sql);
+            stmt2.setString(1, user.getUsername());
+            stmt2.setString(2, user.getPassword());
+            stmt2.setString(3, user.getCountry());
+            stmt2.executeUpdate();
+            stmt2.close();
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         } finally {
@@ -199,9 +121,339 @@ public class User {
                 
             }
         }
-	}
-    
 }
+
+//Follow user
+    public static void followUser(User user, String follow_user) {
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT * FROM AppUser WHERE username=?";
+        String sql = "INSERT INTO Followers (followedId, followerId) VALUES(?, ?);";
+        try {
+			con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query);
+            stmt1.setString(1, follow_user);
+            ResultSet rs = stmt1.executeQuery();
+            if (!rs.next()) {
+                rs.close();
+                stmt1.close();
+                db.close();
+                throw new Exception ("there is no user with username:" + follow_user);
+            }
+            int follow_id = rs.getInt("userId");
+            PreparedStatement stmt2 = con.prepareStatement(sql);
+			stmt2.setInt(1, follow_id);
+			stmt2.setInt(2, user.getId());
+			stmt2.executeUpdate();
+			stmt2.close();
+        } catch (Exception e) {
+           
+        } finally {
+        try {
+            db.close();
+        } catch (Exception e) {
+            
+        }
+    }
+        
+    } 
+
+//Unfollow user
+    public static void unfollowUser(User user, String unfollow_user) {
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT * FROM AppUser WHERE username=?;";
+        String sql = "DELETE FROM Followers WHERE followedId = ? AND followerId = ?;";
+        try {
+			con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query);
+            stmt1.setString(1, unfollow_user);
+            ResultSet rs = stmt1.executeQuery();
+            if (!rs.next()) {
+                rs.close();
+                stmt1.close();
+                db.close();
+                throw new Exception ("there is no user with username:" + unfollow_user);
+            }
+            int unfollow_id = rs.getInt("userId");
+            PreparedStatement stmt2 = con.prepareStatement(sql);
+			stmt2.setInt(1, unfollow_id);
+			stmt2.setInt(2, user.getId());
+			stmt2.executeUpdate();
+			stmt2.close();
+        } catch (Exception e) {
+           
+        } finally {
+            try {
+            db.close();
+            } catch (Exception e) {
+            
+            }
+        }    
+    } 
+//Get Followings Method
+    public static List<String> getFollowing(User user) throws Exception {
+        List<String> followings = new ArrayList<String>();
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT username FROM AppUser JOIN Followers ON "  
+                        +"Followers.followedId = AppUser.userid "
+                        +"WHERE followerId=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, user.getId());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                followings.add(rs.getString("username"));
+            }
+            stmt.close();
+            rs.close();
+            db.close();
+            return followings;
+        } catch (Exception e) {
+            throw new Exception (e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+            
+            }
+        }    
+    } 
+
+//Get Followers Method
+    public static List<String> getFollowers(User user) throws Exception {
+        List<String> followers = new ArrayList<String>();
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT username FROM AppUser JOIN Followers ON "  
+                        +"Followers.followerId = AppUser.userid "
+                        +"WHERE followedId=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, user.getId());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                followers.add(rs.getString("username"));
+            }
+            stmt.close();
+            rs.close();
+            db.close();
+            return followers;
+        } catch (Exception e) {
+            throw new Exception (e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+            
+            }
+        }    
+    }
+//Create List Method
+    public static void createList(String listType, String listName, User user) throws Exception {
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT * FROM list WHERE name=? AND userId=?;";
+        String sql = "INSERT INTO List(listType, name, userId) VALUES (?,?,?)";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query);
+            stmt1.setString(1, listName);
+            stmt1.setInt(2,user.getId());
+            ResultSet rs = stmt1.executeQuery();
+            if (rs.next()) {
+                rs.close();
+                stmt1.close();
+                throw new Exception("List " + listName + " already exists");
+            }
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1,listType);
+            stmt.setString(2, listName);
+            stmt.setInt(3,user.getId());
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+//Get List Method
+    public static List<String> getLists(User user) throws Exception {
+        List<String> lists = new ArrayList<String>();
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT DISTINCT name FROM List WHERE userId=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, user.getId());
+            ResultSet rs = stmt.executeQuery();
+           
+            while(rs.next()) {
+                lists.add(rs.getString("name"));
+            }
+            rs.close();
+            stmt.close();
+            return lists;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+            
+            }
+        }
+    }
+
+// Add to List Method 
+    public static void addToList(String listName, String movieName, String movieId, User user) throws Exception {
+        int listId;
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT list_id FROM List WHERE name=? AND userId=?;";
+        String sql = "INSERT INTO MoviesList (list_id, movieName, movieId) VALUES (?,?,?);";
+            try {
+                con = db.getConnection();
+                PreparedStatement stmt = con.prepareStatement(query);
+                stmt.setString(1, listName);
+                stmt.setInt(2,user.getId());
+                ResultSet rs = stmt.executeQuery();
+                rs.next();
+                listId = rs.getInt("list_id");
+                rs.close();
+                stmt.close();
+                PreparedStatement stmt1 = con.prepareStatement(sql);
+                stmt1.setInt(1, listId);
+                stmt1.setString(2, movieName);
+                stmt1.setString(3, movieId);
+                stmt1.executeUpdate();
+                stmt1.close();
+                } catch (Exception e) {
+                    throw new Exception(e.getMessage());
+            } finally {
+                try {
+                    db.close();
+                } catch (Exception e) {
+                
+                }
+            }
+    }
+    
+// Get Movies From List Method
+    public static List<String> getMoviesFromList(String listName, User user) throws Exception {
+        int listId;
+        List<String> movies = new ArrayList<String>();
+        DB db = new DB();
+        Connection con = null;
+        String query1 = "SELECT list_id FROM List WHERE name=? AND userId=?;";
+        String query2 = "SELECT movieName FROM MoviesList WHERE list_id=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query1);
+            stmt1.setString(1, listName);
+            stmt1.setInt(2, user.getId());
+            ResultSet rs1 = stmt1.executeQuery();
+            rs1.next();
+            listId = rs1.getInt("list_id");
+            rs1.close();
+            stmt1.close();
+            PreparedStatement stmt2 = con.prepareStatement(query2);
+            stmt2.setInt(1, listId);
+            ResultSet rs2 = stmt2.executeQuery();
+            while (rs2.next()) {
+                movies.add(rs2.getString("movieName"));
+            }
+            stmt2.close();
+            rs2.close();
+            return movies;
+        } catch (Exception e) {
+                    throw new Exception(e.getMessage());
+        } finally {
+                try {
+                    db.close();
+                } catch (Exception e) {
+                
+                }
+        }
+    }
+// Delete List Method
+    public static void deleteList(String listName, User user) throws Exception {
+        DB db = new DB();
+        Connection con = null;
+        String sql = "DELETE FROM list WHERE userId=? AND name=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, user.getId());
+            stmt.setString(2, listName);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+// Remove movie from List Method
+    public static void removeMovie(String movieName, String listName, User user) throws Exception {
+        int listId;
+        DB db = new DB();
+        Connection con = null;
+        String query = "SELECT list_id FROM List WHERE name=? AND userId=?;";
+        String sql = "DELETE FROM MoviesList WHERE list_id=? AND movieName=?;";
+        try {
+            con = db.getConnection();
+            PreparedStatement stmt1 = con.prepareStatement(query);
+            stmt1.setString(1, listName);
+            stmt1.setInt(2, user.getId());
+            ResultSet rs1 = stmt1.executeQuery();
+            rs1.next();
+            listId = rs1.getInt("list_id");
+            rs1.close();
+            stmt1.close();
+            PreparedStatement stmt2 = con.prepareStatement(sql);
+            stmt2.setInt(1, listId);
+            stmt2.setString(2, movieName);
+            stmt2.executeUpdate();
+            stmt2.close();
+             } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
