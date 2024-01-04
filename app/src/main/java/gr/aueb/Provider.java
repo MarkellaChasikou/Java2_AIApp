17f44ac89d837d12eeefba296133afd00dcecbd2
NickRagousis <t8210127@aueb.gr>
2024-01-04 15:50:41 +0200

    Renamed Prov to Provider

---
+++ app/src/main/java/gr/aueb/Provider.java
@@ -0,0 +1,18 @@
+package gr.aueb;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Prov {
+    @SerializedName("provider_name")
+    private String provider_name;
+    
+    public String getProvider_name() {
+        return provider_name;
+    }
+
+    @Override
+    public String toString() {
+        return provider_name;
+    }
+    
+}
