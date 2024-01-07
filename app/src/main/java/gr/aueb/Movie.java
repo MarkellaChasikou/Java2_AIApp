2d44585be26123657f49cab3375f6185e2d2ba76
NickRagousis <t8210127@aueb.gr>
2024-01-07 19:18:11 +0200

    Added method to form toString, moved search to App

--- app/src/main/java/gr/aueb/Movie.java
+++ app/src/main/java/gr/aueb/Movie.java
@@ -167,45 +167,120 @@ public class Movie {
         }
     }
     
-    @Override
-    public String toString() {
-        StringBuilder gens = new StringBuilder();
-        StringBuilder ca = new StringBuilder();
-        StringBuilder cr = new StringBuilder();
-
-        // Genres
-        Genre[] genres = this.md.getGenres();
-        for (int i = 0; i < genres.length; i++) {
-            gens.append(genres[i]);
-            if (i < genres.length - 1) {
-                gens.append(", ");
+    private ArrayList<String> getDirectors() {
+        ArrayList<String> directors = new ArrayList<>();
+        if(this.co.getCrew() != null) {
+            for (Crew c : this.co.getCrew()) {
+                if(c.getJob().equals("Director")) {
+                    directors.add(c.getName());
+                }
             }
         }
+        return directors;
+    }
 
-        // Cast
-        for (Cast c : this.co.getCast()) {
-            ca.append(c);
+    private ArrayList<String> getWriters() {
+        ArrayList<String> writers = new ArrayList<>();
+        if(this.co.getCrew() != null) {
+            for (Crew c : this.co.getCrew()) {
+                if(c.getJob().equals("Writer")) {
+                    writers.add(c.getName());
+                }
+            }
+        }
+        return writers;
+    }
+    
+    private ArrayList<String> getActors() {
+        ArrayList<String> actors = new ArrayList<>();
+        if(this.co.getCast() != null) {
+            if(this.co.getCast().length >= 4) {
+                actors.add(this.co.getCast()[0].getName() + " (" + this.co.getCast()[0].getCharacter() + ")");
+                actors.add(this.co.getCast()[1].getName() + " (" + this.co.getCast()[1].getCharacter() + ")");
+                actors.add(this.co.getCast()[2].getName() + " (" + this.co.getCast()[2].getCharacter() + ")");
+                actors.add(this.co.getCast()[3].getName() + " (" + this.co.getCast()[3].getCharacter() + ")");
+            } else {
+                for (Cast c : this.co.getCast()) {
+                    actors.add(c.getName() + " (" + this.co.getCast()[0].getCharacter() + ")");
+                }
+            }
+        }
+        return actors;
+    }
+    
+    private String printResult() {
+        StringBuilder returnString = new StringBuilder();
+        if(this.md.getOriginal_title() != null) {
+            returnString.append("Title: " + this.md.getOriginal_title() + "\n\n");
+        }
+        
+        if(this.md.getOverview() != null) {
+            returnString.append(this.md.getOverview() + "\n \n");
         }
 
-        // Crew
-        for (Crew c : this.co.getCrew()) {
-            cr.append(c);
+        if(this.md.getGenres() != null) {
+            returnString.append("Genres: ");
+            Genre[] genres = this.md.getGenres();
+            for (int i = 0; i < genres.length; i++) {
+                returnString.append(genres[i]);
+                if (i < genres.length - 1) {
+                    returnString.append(", ");
+                }
+            }
+            returnString.append("\n\n");
         }
 
-        return "\n" + this.md.getOverview() + "\n \n"
-            + "Title: " + this.md.getOriginal_title() + "\n"
-            + "Runtime: " + this.md.getRuntime() + "m" + "\n"
-            + "Genres: " + gens + "\n"
-            + "Release Date: " + this.md.getRelease_date() + "\n"
-            + "Tmdb Rating: " + this.md.getVote_average() + "\n"
-            //+ "Imdb Rating: " + this.md.getImdbRatingFromID(this.md.getImdb_id()) + "\n \n \n"
-            + "Imdb Rating: " + this.getImdbRating() + "\n \n \n" 
-            + "Movie Contributors: \n \n"
-            + "Cast: \n"
-            + ca + "\n \n"
-            + "Crew: \n"
-            + cr + "\n\n";
-            //+ this.av.toString(null)
+        if(this.getImdbRating() != -1) returnString.append("Imdb Rating: " + this.getImdbRating() + "\n \n");
+        
+        if(this.md.getRuntime() != null) {
+            returnString.append("Runtime: " + this.md.getRuntime() + "m" + "\n\n");
+        }
+
+        if(this.md.getRelease_date() != null) {
+            returnString.append("Release Date: " + this.md.getRelease_date() + "\n\n");
+        }
+
+        if(getDirectors() != null) {
+            returnString.append("Director(s): ");
+            for (int i = 0; i < getDirectors().size(); i++) {
+                returnString.append(getDirectors().get(i));
+                if (i < getDirectors().size() - 1) {
+                    returnString.append(", ");
+                }
+            }
+            returnString.append("\n\n");
+        }
+
+        if(getWriters() != null) {
+            returnString.append("Writer(s): ");
+            for (int i = 0; i < getWriters().size(); i++) {
+                returnString.append(getWriters().get(i));
+                if (i < getWriters().size() - 1) {
+                    returnString.append(", ");
+                }
+            }
+            returnString.append("\n\n");
+        }
+
+        if(getActors() != null) {
+            returnString.append("Top billed cast:\n");
+            for (int i = 0; i < getActors().size(); i++) {
+                returnString.append(getActors().get(i));
+                if (i < getActors().size() - 1) {
+                    returnString.append("\n");
+                }
+            }
+            returnString.append("\n\n");
+        }
+
+        returnString.append(av.toString("GR")); //temp
+        return returnString.toString();
+    }
+
+    @Override
+    public String toString() {
+        String s = printResult();
+        return s;
     }
 
 
