2883aec1b33a1823b6fb4f1014c4e5523b12322c
NickRagousis <t8210127@aueb.gr>
2024-01-08 21:58:01 +0200

    added getGenres to get all existing genres

--- app/src/main/java/gr/aueb/Genre.java
+++ app/src/main/java/gr/aueb/Genre.java
@@ -1,11 +1,57 @@
 package gr.aueb;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.annotations.SerializedName;
 
 public class Genre {
     @SerializedName("name")
     private String name;
+    @SerializedName("id")
+    private int id;
 
+    public static HashMap<Integer, String> getGenres(String apiKey) {
+        HashMap<Integer, String> genres = new HashMap<>();
+        
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.themoviedb.org/3/genre/movie/list?language=en"))
+            .header("accept", "application/json")
+            .header("Authorization", "Bearer " + apiKey)
+            .method("GET", HttpRequest.BodyPublishers.noBody())
+            .build();
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            JsonElement element = JsonParser.parseString(response.body());
+            JsonObject jsonObject = element.getAsJsonObject();
+            JsonArray resultsArray = jsonObject.getAsJsonArray("results");
+
+            for (JsonElement g : resultsArray) {
+                JsonObject countryObject = g.getAsJsonObject();
+                int id = countryObject.get("id").getAsInt();
+                String name = countryObject.get("name").getAsString();
+
+                genres.put(id, name);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return genres;
+    }
+    
     public String getName() {
         return name;
     }
