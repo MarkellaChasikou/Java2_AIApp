6e3c5da132a6065c6f90fe624adf6c6ad931e54b
NickRagousis <t8210127@aueb.gr>
2024-01-17 14:57:28 +0200

    small change

--- app/src/main/java/gr/aueb/MovieDAO.java
+++ app/src/main/java/gr/aueb/MovieDAO.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Data Access Object (DAO) for Movie-related database operations.
