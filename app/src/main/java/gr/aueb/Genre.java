8bf9f27a7fc8a8c8f21c9ed37f843584c3f8ff3e
NickRagousis <t8210127@aueb.gr>
2024-01-24 17:34:29 +0200

    all genres method deleted

--- app/src/main/java/gr/aueb/Genre.java
+++ app/src/main/java/gr/aueb/Genre.java
@@ -5,16 +5,6 @@
  */
 package gr.aueb;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 
 /**
@@ -42,45 +32,6 @@ public class Genre {
     /** The unique identifier for the genre. */
     @SerializedName("id")
     private int id;
-
-    /**
-     * Retrieves a mapping of genre IDs to names using The Movie Database (TMDb)
-     * API.
-     * 
-     * @param apiKey The API key for TMDb authentication.
-     * @return A HashMap containing genre IDs as keys and corresponding names as
-     *         values.
-     */
-    public static HashMap<Integer, String> getGenres(String apiKey) {
-        HashMap<Integer, String> genres = new HashMap<>();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/genre/movie/list?language=en"))
-                .header("accept", "application/json")
-                .header("Authorization", "Bearer " + apiKey)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
-            JsonArray genresArray = jsonObject.getAsJsonArray("genres");
-            
-            for (JsonElement genreElement : genresArray) {
-                JsonObject genreObject = genreElement.getAsJsonObject();
-                int id = genreObject.get("id").getAsInt();
-                String name = genreObject.get("name").getAsString();
-                genres.put(id, name);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return genres;
-    }
 
     /**
      * Gets the name of the genre.
